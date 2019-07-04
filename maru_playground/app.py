51b630cb06214d4dfb554291e4cd8b4a3ef56e85
Maximux <fournes.maxime@gmail.com>
2019-07-05 00:13:28 +0100

    Initial commit

---
+++ maru_playground/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify, Response
+import subprocess
+
+app = Flask(__name__, template_folder="static")
+
+MARU_PATH = "./maru"
+
+def run_code(code: str) -> str:
+    return subprocess.check_output([MARU_PATH, "-c", code])
+
+@app.route('/')
+def send_index():
+    return render_template("index.html")
+
+@app.route("/", methods=["POST"])
+def compute():
+    code = request.form["code"]
+    output = run_code(code).decode()
+    print(output)
+    return render_template("index.html", input=code, output=output)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000, True)
